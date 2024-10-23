6544940462cffd365b22af6e6d3dfdf22eece703
Vijay Chauhan <myselfvijaykumar@gmail.com>
2024-10-23 12:23:05 +0530

    React : lesson 1

---
+++ app.js
@@ -0,0 +1,4 @@
+let heading = React.createElement("h1",{ id: "heading", xyz: "abc" }, "Hello World from React!" );
+let root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading); 
